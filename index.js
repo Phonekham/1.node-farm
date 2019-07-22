d4e622f6511cf3f623d8d7d785d1b16c519d684d
Phonekham <phonekham.dev@gmail.com>
2019-07-22 18:07:31 +0700

    npm

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const slugify = require("slugify");
 const replaceTemplate = require("./modules/replaceTemplate");
-
-// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-// console.log(textIn);
-//
-// const textOut = `This is what we know: ${textIn}.\nCreated on ${Date.now()}`;
-// fs.writeFileSync("./txt/output.txt", textOut);
-// console.log("File Writen");
 
 // SERVER
 const tempOverview = fs.readFileSync(
@@ -25,6 +19,9 @@ const tempProduct = fs.readFileSync(
 );
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
